bde56f3a67c8074690f05da14ace9c63c280ef01
erikquijano <63459405+erikquijano@users.noreply.github.com>
2024-05-11 13:02:49 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.sidebar.success("Select a demo above.")
 
 # Function to load data
 @st.cache
